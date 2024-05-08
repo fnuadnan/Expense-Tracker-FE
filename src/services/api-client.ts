6b9448049eb3560de944af79b9fa636f939197a1
Fnu Adnan <129648022+fnuadnan@users.noreply.github.com>
2024-05-08 01:36:08 -0500

    Update API base URL

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://aqueous-savannah-37526-035c93d4e2b1.herokuapp.com/api",
 });
