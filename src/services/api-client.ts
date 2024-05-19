d88670f4cecda816814f9f1a5b7f34ae61096979
Fnu Adnan <129648022+fnuadnan@users.noreply.github.com>
2024-05-18 22:10:08 -0500

    Update API base URL

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://expense-tracker-fe-omega.vercel.app/api",
+  baseURL: "https://mysterious-bayou-33240-c7cba7d0e9e5.herokuapp.com/api",
   withCredentials: true,
 });
